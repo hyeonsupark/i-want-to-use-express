5f7f8a6367ca4c88b3f22bcabe21d01174efb347
jangjunha <jangjunha113@gmail.com>
2016-09-25 20:08:46 +0900

    Add socket.io chatting server.

---
+++ chatsocket/index.js
@@ -0,0 +1,34 @@
+module.exports = function (http, connection) {
+  // Initialize Socket.io
+  var io = require('socket.io')(http).listen(3333);
+
+  // Initialize MySQL Connection
+  connection.connect();
+
+  // On Connect
+  io.on('connection', function(socket) {
+    console.log('### A user connected');
+
+    // On recieve message
+    // Example
+    var ex = {
+      'nickname': 'Hyeonsu',
+      'message': 'ㅓ어아!! 난 현수얌'
+    }
+    socket.on('send message', function(msg) {
+      // Logging
+      console.log('# Message: ' + msg);
+      connection.query({
+        'query': 'INSERT INTO `chat` (`nickname`, `message`) VALUES (?, ?)',
+        'values': [ msg['nickname'], msg['message'] ],
+        'timeout': 10000
+      }, function (error, results, fields) {
+      });
+
+      // BR
+      io.emit('recieve message', msg);
+    });
+  });
+
+  return io;
+};
